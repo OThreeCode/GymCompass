d79deef6239c8150bd6ce8a736a92a6223abc39d
Lucas Dantas <lucasdantas.dev@gmail.com>
2021-02-24 00:28:12 -0300

    Logout button.

--- resources/views/layouts/authenticated.blade.php
+++ resources/views/layouts/authenticated.blade.php
@@ -87,6 +87,12 @@
                            </a>
                         </nav>
                      </div>
+                     <a href="/logout" class="group rounded-md py-2 px-4 mb-2 flex items-center text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+                        <svg class="ml-2 mr-3 h-6 w-6 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                        </svg>
+                        Sair
+                     </a>
                      <div class="flex-shrink-0 flex bg-gray-700 p-4">
                         <a href="#" class="flex-shrink-0 w-full group block">
                            <div class="flex items-center">

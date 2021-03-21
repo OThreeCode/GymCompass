39b4377c3e27f8b5e643203536eace5787dd5978
Rayan Avelino <rayanodb@gmail.com>
2021-03-20 22:44:03 -0300

    Fix client error after login

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,6 +17,7 @@ class DashboardController extends Controller
         } else {
             $users = null;
         }
+        
         return view('home', [
             'users' => $users,
         ]);
